183dd1a892e1f42a2ca12adb9fd1b4ceaba74f6f
jacky2code <jacky2code@gmail.com>
2023-02-16 22:47:09 +0800

    Initial Commit

---
+++ Little Lemon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Little Lemon
+//
+//  Created by jackys on 2023/2/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
