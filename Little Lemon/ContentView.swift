8a4da5cc46881d900a5a2e0aad2de32b393495e6
jacky2code <jacky2code@gmail.com>
2023-02-17 10:54:59 +0800

    Controls

--- Little Lemon/ContentView.swift
+++ Little Lemon/ContentView.swift
@@ -8,30 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isShowing = true
+    @State private var value = 1
+    @State private var size: CGFloat = 0.1
+    @State var selectedDate = Date()
+    var dateClosedRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+        let max = Calendar.current.date(byAdding: .day, value: 30, to: Date())!
+        return min...max
+    }
+
     var body: some View {
-        ZStack(alignment: .leading) {
-                    Circle()
-                        .foregroundColor(Color.gray)
-                    Circle()
-                        .scale(x: 0.75, y: 0.75)
-                        .foregroundColor(Color.red)
-                    VStack {
-                        Image(systemName: "globe")
-                            .imageScale(.large)
-                            .foregroundColor(.accentColor)
-                        Text ("Hello, world!")
-                        Spacer()
-                        HStack(spacing: 20) {
-                            Button("One"){}
-                            Button("Two"){}
-                            
-                        }
-                        .frame(maxHeight: .infinity, alignment: .top)
-                        
-                    }
-                    .frame(maxWidth: .infinity, alignment: .trailing)
-                    
-                }
+//        Button(role: .destructive) {
+//            print("do something")
+//        } label: {
+//            HStack {
+//                Image(systemName: "trash")
+//                Text("")
+//            }
+//        }
+//
+//        Toggle(isOn: $isShowing) {
+//            Text("Hello world!")
+//        }
+//
+//        NavigationView {
+//            VStack {
+//                Text("Current value: \(value)")
+//                Stepper("Number of guests", value: $value, in:1...20)
+//            }
+//            .padding()
+//            .navigationTitle("Reservation form")
+//        }
+//
+//        VStack {
+//
+//            Text("Little Lemon").font(.system(size: size * 50))
+//            Slider(value: $size)
+//        }
+//        .padding()
+        
+//        NavigationView {
+//                    Form {
+//                        Section {
+//                            DatePicker(
+//                                selection: $selectedDate,
+//                                in: dateClosedRange,
+//                                displayedComponents: .date,
+//                                label: { Text("Due Date") }
+//                            )
+//                        }
+//                    }
+//                }
+        
+        Label("Menu", systemImage: "fork.knife")
+                    .labelStyle(.titleAndIcon)
+
     }
 }
 
